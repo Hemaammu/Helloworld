c866086fd871b70cc0fdbaf0e33e5981b2d34dad
Hemaammu <hemaammu@github>
2018-11-13 11:28:57 +0530

    third commit

--- helloworld/src/test/java/helloworld/helloworld/AppTest.java
+++ helloworld/src/test/java/helloworld/helloworld/AppTest.java
@@ -15,6 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
+	
     public AppTest( String testName )
     {
         super( testName );
@@ -34,6 +35,5 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
-        System.out.println("Hello world");
-    }
+      }
 }
